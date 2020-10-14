ae4527b43d8081bb4aa98112fa4156abae28de4e
Bernardo A. Rodrigues <bernardoaraujor@gmail.com>
2020-10-14 17:33:20 +0100

    sanitize main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-extern crate linux_embedded_hal as hal;
-extern crate hdc20xx;
-extern crate opt300x;
-extern crate nb;
-
 extern crate coral_env;
 
 fn main() {
-    println!("Temperature: {}°C", coral_env::temperature());
-    println!("Humidity: {}%", coral_env::humidity());
+    println!("Temperature: {} °C", coral_env::temperature());
+    println!("Humidity: {} %", coral_env::humidity());
     println!("Ambient Light: {} lux", coral_env::light());
     println!("Pressure: {} kPa", coral_env::pressure());
 }
